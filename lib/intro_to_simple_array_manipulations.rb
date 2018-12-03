3ae495de02ac8ed1ee22a5d68b185d15892ca142
jesusg1012 <jesus.jg.medina@gmailcom>
2018-12-03 23:55:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, word)
+  array.push(word)
+end
+def using_unshift(array, word)
+  array.unshift(word)
+end
+def using_pop(array)
+  array.pop
+end
+def pop_with_args(array)
+  arr = []
+arr.push(array.pop)
+arr.unshift(array.pop)
+end
+def using_unshift(array)
+  array.shift
+end
+def shift_with_args(array)
+  arr = []
+  arr.push(arry.shift)
+  arr.push(arry.shift)
+end
